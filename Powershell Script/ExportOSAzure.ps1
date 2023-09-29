43717829b3e5d546a0e4633848fd2af04be7b134
N1oks <edson.fagundes@outlook.com>
2023-09-29 10:24:32 +0200

    .

---
+++ Powershell Script/ExportOSAzure.ps1
@@ -0,0 +1,32 @@
+﻿#########################################################################
+# This content is licensed over the Creative Commons                    #
+# https://creativecommons.org/publicdomain/zero/1.0/?ref=chooser-v1     #
+# This script were created by Edson Fagundes                            # 
+#########################################################################
+# The script have as objective export the OS version of VMs.            # 
+#########################################################################
+
+Connect-AZAccount 
+
+Get-AzSubscription | 
+#Where-Object Name -like "*DEV*"|
+#In case you would like to filter the Subscriptions you can use the preview line, removing the comment. 
+
+ForEach-Object {
+    $subscriptionName = $_.Name
+    echo $subscriptionName
+    $result = az vm list --subscription $subscriptionName --query "[].[name,storageProfile.imageReference.offer,storageProfile.imageReference.sku]" 
+    $json = $result | ConvertFrom-Json
+
+    $json | % {
+        $item = New-Object PSCustomObject
+        $item | Add-Member -NotePropertyName "vmName" -NotePropertyValue $_.SyncRoot[0]
+        $item | Add-Member -NotePropertyName "Os" -NotePropertyValue $_.SyncRoot[1]
+        $item | Add-Member -NotePropertyName "sku" -NotePropertyValue $_.SyncRoot[2]
+
+        $item | Export-Csv .\servers.csv  -NoTypeInformation  -Append
+    }
+
+}
+  
+
---
+++ Powershell Script/Export_AKS_OS.ps1
@@ -0,0 +1,40 @@
+﻿#########################################################################
+# This content is licensed over the Creative Commons                    #
+# https://creativecommons.org/publicdomain/zero/1.0/?ref=chooser-v1     #
+# This script were created by Edson Fagundes                            # 
+#########################################################################
+# The script have as objective export the OS version of all AKS pool.   # 
+#########################################################################
+
+Connect-AZAccount  
+
+$ErrorActionPreference =  "Stop"  
+try  {  
+Get-AzSubscription | 
+#Where-Object Name -like "*DEV*"|
+#In case you would like to filter the Subscriptions you can use the preview line, removing the comment. 
+
+ForEach-Object {
+    $subscriptionName = $_.Name
+    Set-AzContext  -SubscriptionName  $_.Name 
+    $rgs =  Get-AzResourceGroup  
+    foreach  ($rg in $rgs.ResourceGroupName){ 
+        if(Get-AzAksCluster){
+             $Cluster = Get-AzAksCluster
+             if($cluster.Name){
+                foreach($unicName in $cluster){
+                    Get-AzAksNodePool -ResourceGroupName $unicName.ResourceGroupName  -ClusterName $unicName.Name |  Select  Id, OsType,  OsSKU, OrchestratorVersion, CurrentOrchestratorVersion  |  Export-Csv  .\ContainerOS.csv -Append  -Force  -NoTypeInformation  
+                }
+            
+            }
+        }
+        
+    }  
+  }
+}  
+
+catch  {  
+    Write-Host  "$($_.Exception.Message)"  -BackgroundColor  DarkRed  
+}
+
+     
